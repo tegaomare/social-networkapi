a142fe529f03991a77b1b230220724c01436f1d3
tegaomare <tegaomare@yahoo.com>
2023-07-16 12:55:02 -0400

    api removed

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require("mongoose");
 
-connect("mongodb://127.0.0.1:27017/social-network-api");
+connect("mongodb://127.0.0.1:27017/social-network");
 
 module.exports = mongoose.connection;

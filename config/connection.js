f5324dc791c38b86548637cbc0b4a193bbcd6e89
tegaomare <tegaomare@yahoo.com>
2023-07-16 13:17:32 -0400

    .env added

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,14 @@
-const { connect, connection } = require("mongoose");
+const mongoose = require("mongoose");
 
-connect("mongodb://127.0.0.1:27017/social-network");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
 
 module.exports = mongoose.connection;
